8a74581d1a882545b77191459bbfbca316873954
Oliver Oldfield-Hodge <oliver@oldfield-hodge.com>
2017-05-19 11:28:07 +1000

    Hello world and build fixes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 import io.featureflow.client.FeatureflowClient;
 import io.featureflow.client.FeatureflowContext;
+import io.featureflow.client.model.Feature;
 
 import java.io.IOException;
 
@@ -8,6 +9,7 @@ public class HelloWorld {
     public static void main(String... args) throws IOException {
         //This is the simplest possible invocation of the featureflow client:
         FeatureflowClient client = FeatureflowClient.builder("{{YOUR_SERVER_ENVIRONMENT_API_KEY_HERE}}")
+                .withFeature(new Feature(MyFeatures.EXAMPLE_FEATURE.getValue()))
                 .withUpdateCallback(control -> System.out.println("Received a control update event: " + control.getKey() + " variant: " + control.evaluate(FeatureflowContext.context().build())))
                 .build();
         //we call evaluate here to get the variant, which will be the evaluated variant value as per your rules - or 'off' in the case of a failover situation
