482e519edd8aa0ae7550fe227e517e51f3124313
Oliver Oldfield-Hodge <oliver@oldfield-hodge.com>
2017-07-18 12:11:40 +1000

    FEAT-450 Tidy example

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,22 +4,26 @@ import io.featureflow.client.model.Feature;
 
 import java.io.IOException;
 
+/**
+ * This is the featureflow 5 Minute test example. Please see [http://docs.featureflow.io/v1.0/docs/featureflow-five-minute-java] and follow the steps.
+ */
 public class HelloWorld {
 
     public static void main(String... args) throws IOException {
         //This is the simplest possible invocation of the featureflow client:
         FeatureflowClient client = FeatureflowClient.builder("{{YOUR_SERVER_ENVIRONMENT_API_KEY_HERE}}")
-                .withFeature(new Feature(MyFeatures.EXAMPLE_FEATURE.getValue()))
+                .withFeature(new Feature(MyFeatures.EXAMPLE_FEATURE.getValue()))   //we register features up front. that lowers technical debt by allowing configuration in one place (hint enums + IDE help find all usages!).
                 .withUpdateCallback(control -> System.out.println("Received a control update event: " + control.getKey() + " variant: " + control.evaluate(FeatureflowContext.context().build())))
                 .build();
-        //we call evaluate here to get the variant, which will be the evaluated variant value as per your rules - or 'off' in the case of a failover situation
-        //to change the failover see the HelloWorldWithContext example
+
+        //evaluate the variant here. by convention the default is off unless you set a failover when registering the feature above.
         if (client.evaluate(MyFeatures.EXAMPLE_FEATURE.getValue()).isOn()) {
             System.out.println("The variant is on!");
         } else {
             System.out.println("The variant is not on!");
         }
         client.close();
+
         //Now have a look at HelloWorldWithContext.java for a more advanced scenario
     }
 }
--- src/main/java/HelloWorldWithContext.java
+++ src/main/java/HelloWorldWithContext.java
@@ -6,29 +6,29 @@ import org.joda.time.DateTime;
 import java.io.IOException;
 import java.util.Arrays;
 
+/**
+ * This is the featureflow 5 Minute test example. Please see [http://docs.featureflow.io/v1.0/docs/featureflow-five-minute-java] and follow the steps.
+ */
 public class HelloWorldWithContext {
 
     public static void main(String... args) throws IOException {
-        /*
-            This is an example of evaluating a feature by passing in some context information
-            Continue down the comments below for an explanation.
-         */
+        //Create a context object. The context tells featureflow about the current user and any other information you may wish to target
 
-        /*
-            This is a context, in a web facing application you will generally create this when your users' request (eg login, rest call etc.) comes in.
-            If you do not have a logged in user or any context that's ok, we will assume the user is 'anonymous'
-            We have a builder just to aid generating the context, you will then pass the context into the evaluate method.
+        FeatureflowClient client = new FeatureflowClient.Builder("{{YOUR_SERVER_ENVIRONMENT_API_KEY_HERE}}")
+                .withFeature(new Feature(MyFeatures.EXAMPLE_FEATURE.getValue()))
+                .withFeature(new Feature(MyFeatures.NEW_FEATURE.getValue()))
+                .build();
 
-            Some quick concepts: A 'context' is evaluated against a features 'rules' to determine which 'variant' of the feature to use for the given context.
-            In the 'HelloWorld' the variants are simple 'On' and 'Off' - in more complex scenarios they can be anything - eg: 'red','blue','1.1','1.2'
-            This allows a huge amount of control, for example:
-                'Set the 'Google Login Button' feature to show the '1.2-NewVersion' variant if the 'context' contains a 'user_role' of 'pvt_tester'
-                'Set the 'Stock Chart' feature to show the 'Enhanced Stock Chart' variant if the 'context' contains a 'tier' of 'gold'
-            Noting that these same rules can be applied using both the backend APIs and the Frontend Javascript API you can toggle conveniently at the front-end while backing up securely with server rules.
+        FeatureflowClient.Evaluate exampleEval = client.evaluate(MyFeatures.EXAMPLE_FEATURE.getValue(), getContext());
+        System.out.println("The variant for " + MyFeatures.EXAMPLE_FEATURE.getValue() + " is " + exampleEval.value());
 
+        FeatureflowClient.Evaluate newFeatureEval = client.evaluate(MyFeatures.NEW_FEATURE.getValue(), getContext());
+        System.out.println("The variant for " + MyFeatures.NEW_FEATURE.getValue() + " is " + newFeatureEval.value());
+        
+        client.close();
+    }
 
-            As these  context values are evaluated they appear in featureflow so that you can select them in 'rules' to target your features.
-         */
+    private static FeatureflowContext getContext(){
         FeatureflowContext context = FeatureflowContext
                 .keyedContext("flo@example.com")
                 .withValue("age", 32)
@@ -36,46 +36,6 @@ public class HelloWorldWithContext {
                 .withValues("user_role", Arrays.asList("admin", "pvt_tester"))
                 .withValue("tier", "gold")
                 .build();
-
-
-        /*
-            We create the featureFlowClient using the builder. The builder helps you instantiate the client with the correct configuration.
-            The client should be a singleton - if your using spring you could create it in your @Configuration and @Inject it, either way you only need one.
-            Here we pass in our 'Server Api Key' - you can get the key in the environments page under the 'Api Keys' link.
-            We are actively registering a feature here with a failover variant of 'standard-variant', we recommend you use a static or enum reference of the key, this will help reduce tech debt and allow you to easily identify usage later
-            We provide a callback function to illustrate how featureflow can react in real-time as features are toggled (generally you would not evaluate on the control directly however)
-         */
-        FeatureflowClient client = new FeatureflowClient.Builder("{{YOUR_SERVER_ENVIRONMENT_API_KEY_HERE}}")
-                .withFeature(new Feature(MyFeatures.EXAMPLE_FEATURE.getValue(), "standard-variant"))
-                .withFeature(new Feature(MyFeatures.NEW_FEATURE.getValue()))
-                .withUpdateCallback(control -> System.out.println("Received a control update event: " + control.getKey() + " variant: " + control.evaluate(context)))
-                .build();
-
-
-        /*
-            'client.evaluate' is en example of evaluating a specific feature variant.
-            The first parameter is the variant key
-            The second parameter is the context created above
-
-            Generally you would call isOn/isOff/value in a chained value, but you can hold the eval object if desired as below.
-         */
-
-        FeatureflowClient.Evaluate eval = client.evaluate(MyFeatures.EXAMPLE_FEATURE.getValue(), context);
-
-        if (eval.isOn()) {
-            System.out.println("The variant is " + eval.value());
-        } else {
-            System.out.println("The variant is " + eval.value());
-        }
-
-        /**
-         * TRY IT! Try editing the 'example-feature' in the environment that matches the key you have set above.
-         * Edit the feature and add a new variant called 'New Version 1.2'
-         * Now edit the control for the environment you are connected to above - Add a new Rule which says when 'tier' is equal to 'gold' then 'New Version 1.2'
-         * You should immediately see an update event which will evaluate to the new version (because your user is in the 'gold' tier in your context above.
-         * Of course for more information and a visual guide please consult the docs http://docs.featureflow.io
-         */
-
-        client.close();
+        return context;
     }
 }
