8d892efcc29cb5912479e656fe6ad57d58be5fea
Oliver Oldfield-Hodge <oliver@featureflow.io>
2020-12-31 09:46:28 +1100

    Update fiveminute example to latest SDK version

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,5 @@
 import io.featureflow.client.FeatureflowClient;
+import io.featureflow.client.FeatureflowUser;
 import io.featureflow.client.model.Feature;
 
 import java.io.IOException;
@@ -9,19 +10,18 @@ import java.io.IOException;
 public class HelloWorld {
 
     public static void main(String... args) throws IOException {
-        //This is the simplest possible invocation of the featureflow client:
+        //Create an instance of the featureflow client - this is typically a singleton (for example a spring bean)
         FeatureflowClient client = FeatureflowClient.builder("{{YOUR_SERVER_ENVIRONMENT_API_KEY_HERE}}")
-                .withFeature(new Feature(MyFeatures.EXAMPLE_FEATURE.getValue()))   //we register features up front. that lowers technical debt by allowing configuration in one place (hint enums + IDE help find all usages!).
+                .withFeature(new Feature(MyFeatures.EXAMPLE_FEATURE.getValue()))   //we can register features up front. that lowers technical debt by allowing configuration in one place (hint enums + IDE help find all usages!).
+                //An update callback may be added to identify when a feature configuration has been updated.
+                .withUpdateCallback(control -> System.out.println("Received a feature control update event: " + control.getKey() + " variant: " + control.evaluate(new FeatureflowUser())))
                 .build();
 
-        //You can add an update callback to evaluate a feature it's configuration is updated
-        //.withUpdateCallback(control -> System.out.println("Received a feature control update event: " + control.getKey() + " variant: " + control.evaluate(new FeatureflowUser())))
-
-        //evaluate the variant here. by convention the default is off unless you set a failover when registering the feature above.
+        //evaluate the variant. by convention the default is off unless you set a failover when registering the feature above.
         if (client.evaluate(MyFeatures.EXAMPLE_FEATURE.getValue()).isOn()) {
-            System.out.println("The variant is on!");
+            System.out.println("The " + MyFeatures.EXAMPLE_FEATURE + " variant is on!");
         } else {
-            System.out.println("The variant is not on!");
+            System.out.println("The " + MyFeatures.EXAMPLE_FEATURE + " variant is not on! ( the value is '" +  client.evaluate(MyFeatures.EXAMPLE_FEATURE.getValue()).value() + "')");
         }
         client.close();
 
